3865cc4842b600f2a3adee205c2e849279c343c7
Dominique De León <dominique.a.deleon@gmail.com>
2019-06-23 04:56:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)
 end
 
 def summon_captain_planet(veggies)
-  veggies.map {|e| e + "!"}
+  veggies.map {|e| e.upcase + "!"}
 end
 
 def long_planeteer_calls# code an argument here

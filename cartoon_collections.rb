af0440e617a583b53db7b698eba96ed14009ba56
Dominique De León <dominique.a.deleon@gmail.com>
2019-06-23 04:57:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(veggies)
   veggies.map {|e| e.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? {|c| c.size > 4 }
 end
 
 def find_the_cheese# code an argument here
